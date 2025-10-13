7bd68a2479e7bed2ba37583bf993d7ea6fcc9df0
webdo-username <webdo-email@example.com>
2025-10-13 23:31:35 +0900

    chlwhd?

--- next.config.js
+++ next.config.js
@@ -1,2 +1,18 @@
-// const nextConfig = {};
-// module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
+    },
+  },
+};
+
+module.exports = nextConfig;
+
